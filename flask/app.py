b24761fa69624a315249aa09455bb15b70d2d5c4
cgp <2986779260@qq.com>
2024-07-10 23:41:01 +0800

    fastapi init

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # 定义预测API接口
 @app.route('/')
 def predict():
-    return "hello flask"
+    return render_template('base.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
